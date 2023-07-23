5f701bfb5e50b78073ae1308cab4b2433f76973f
shipo3 <yfb13555@nifty.com>
2023-07-23 09:35:18 +0000

    serverspec実行

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,3 +1,20 @@
-equire 'serverspec'
+require 'serverspec'
+require 'net/ssh'
 
-set :backend, :exec
+set :backend, :ssh
+set :host, ENV['TARGET_HOST']
+set :ssh_options, {
+  user: ENV['SSH_USER'],
+  keys: [ENV['SSH_KEY_PATH']],
+  forward_agent: true,
+}
+
+RSpec.configure do |c|
+  c.before :all do
+    block = self.class.metadata[:example_group_block]
+    if block
+      file_path = block.source_location.first
+      c.host  = File.basename(File.dirname(file_path))
+    end
+  end
+end
