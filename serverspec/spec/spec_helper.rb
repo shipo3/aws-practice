8f22e46a6bfc7d9a58ce957276f1707cdd03773f
shipo3 <yfb13555@nifty.com>
2023-07-29 09:20:40 +0000

    spec_helper.rbの変更

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -2,19 +2,28 @@ require 'serverspec'
 require 'net/ssh'
 
 set :backend, :ssh
-set :host, ENV['TARGET_HOST']
-set :ssh_options, {
-  user: ENV['SSH_USER'],
-  keys: [ENV['SSH_KEY_PATH']],
-  forward_agent: true,
-}
 
-RSpec.configure do |c|
-  c.before :all do
-    block = self.class.metadata[:example_group_block]
-    if block
-      file_path = block.source_location.first
-      c.host  = File.basename(File.dirname(file_path))
-    end
+# 対象サーバ内でのsudo passwordの設定処理
+# 実行時に 環境変数に ASK_SUDO_PASSWORDが設定されている場合
+# コマンドライン上でパスワードを入力させる
+if ENV['ASK_SUDO_PASSWORD']
+  begin
+    require 'highline/import'
+  rescue LoadError
+    fail "highline is not available. Try installing it."
   end
+  # コマンドライン上でパスワードを入力させる
+  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
+else
+  # ASK_SUDO_PASSWORD設定がオフの場合はここに設定したパスワードを利用する
+  set :sudo_password, ENV['SUDO_PASSWORD']
 end
+host = ENV['TARGET_HOST']
+
+options = Net::SSH::Config.for(host)
+
+# 接続先サーバのユーザ名
+options[:user] ||= 'ec2_user'
+
+set :host,        options[:host_name] || host
+set :ssh_options, options
