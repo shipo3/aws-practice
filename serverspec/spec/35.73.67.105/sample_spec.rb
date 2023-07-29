b35f8b95b1c1f7f5b91118acf1305f93e0c86684
shipo3 <yfb13555@nifty.com>
2023-07-29 06:49:44 +0000

    serverspec実行3

---
+++ serverspec/spec/35.73.67.105/sample_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+listen_port = 80
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe port(listen_port) do
+  it { should be_listening }
+end
+
+# describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
+#   its(:stdout) { should match /^200$/ }
+# end
+
+# describe file('/etc/nginx/nginx.conf') do
+#   it { should be_file }
+#   its(:content) { should match /worker_processes/ }
+# end
