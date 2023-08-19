9ce396000c5552987f8211d2ed09dd53f3475a4c
shipo3 <yfb13555@nifty.com>
2023-08-19 11:51:55 +0000

    テスト内容変更

--- serverspec/spec/52.69.186.67/sample_spec.rb
+++ serverspec/spec/52.69.186.67/sample_spec.rb
@@ -6,9 +6,9 @@ describe package('nginx') do
   it { should be_installed }
 end
 
-describe port(listen_port) do
-  it { should be_listening }
-end
+# describe port(listen_port) do
+#   it { should be_listening }
+# end
 
 # describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
 #   its(:stdout) { should match /^200$/ }
